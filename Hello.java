f023839a5058cae60637b3fe1b7b7d2622093ad9
erick lares <erick_lares@hotmail.com>
2017-02-03 10:44:53 -0500

    Name ready, need idea on test

--- Hello.java
+++ Hello.java
@@ -1,8 +1,16 @@
 public class Hello {
    public static  void main(String [] args) {
         if (args.length  != 0){
-		String username = args[0];
-	
+		String username;
+		String aux = args[0];
+		if(aux.equals("-me")  && args.length>2){	
+			String Pname = args[1] ;
+			username = args[2];
+
+		}
+		else{
+			username = args[0];
+		}
 		System.out.println("Hello, "+ 
 username.substring(0,1).toUpperCase() + username.substring(1));}
 	else{
