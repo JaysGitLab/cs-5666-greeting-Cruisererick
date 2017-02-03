2e14ae13b5fcae5acc659c2d9f8a780946cedc34
erick lares <erick_lares@hotmail.com>
2017-02-03 09:18:35 -0500

    Upper case donde

--- Hello.java
+++ Hello.java
@@ -3,7 +3,8 @@ public class Hello {
         if (args.length  != 0){
 		String username = args[0];
 	
-		System.out.println("Hello, "+ username);}
+		System.out.println("Hello, "+ 
+username.substring(0,1).toUpperCase() + username.substring(1));}
 	else{
 		System.out.println("Hello, Erick");
 	}
